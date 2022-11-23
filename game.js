94c2ce0e4f8c58f8a554a50f69768d669564f23b
tyler ferre <ferre.tyler1@gmail.com>
2022-11-22 18:25:18 -0700

    Fixed walking and random generator. Finished

--- game.js
+++ game.js
@@ -5,15 +5,18 @@ const readline = require("readline-sync");
 const name = readline.question("Please input a username to begin: ", {
     //hideEchoBack: true,
    });
+    console.clear()
+    console.log("")
     console.log(`Hello ${name}, welcome to the Starwars Universe!`)
     console.log("")
-    console.log("In this game you will fight enemies and collect credits to aid the rebellion. ")
-    console.log("(You can check how many you have in your inventory by pressing 'p', as well as your health)")
+    console.log("In this game you will fight enemies and collect credits to aid the rebellion. Get 5 to win!")
+    console.log("(You can check how many you have in your inventory and health by pressing 'p', or press 'x' to quit if you're too scared)")
     console.log("")
     console.log("But be careful.. the empire is near.")
     console.log("")
 
    if(readline.keyInYNStrict("Are you ready to start?")){
+    console.clear()
     console.log("")
     console.log("Press 'w' to walk")
     }else{
@@ -35,6 +38,28 @@ const name = readline.question("Please input a username to begin: ", {
     }
 
 
+    win = () =>{
+        console.log("")
+        console.log("YOU WIN!!")
+        console.log("")
+        console.log("Good work! The rebellion thanks you for your efforts, these credits will go to a good cause.")
+        console.log("")
+        if(readline.keyInYNStrict("Continue for fun?")){
+            console.clear()
+            console.log("")
+            // console.log("Press 'w' to walk")
+            return
+        }else{
+            end()
+            console.clear()
+        }
+    }
+
+    end = () =>{
+        hp = 0
+        return
+    }
+
     die = () =>{
         console.log("YOU DIED")
         console.log("")
@@ -50,16 +75,16 @@ const name = readline.question("Please input a username to begin: ", {
     }
 
 
-    //random enemy genorator
-    randEnemies = (enemy) =>{
-        var randomIndex = Math.floor(Math.random() * enemy.length)
-        var item = enemy[randomIndex]
-        return item
-    }
+    // random enemy genorator
+    // randEnemies = (enemies) =>{
+ 
+    //     // var item = enemy[randomIndex]
+    //     console.log(randomIndex)
+    //     console.log(enemies.includes(undefined))
+    // }
 
+    let enemies = ["Stormtrooper", "Imperial Guard", "Scout Trooper", "Probe Droid", "Imperial Officer", "Shock Trooper", "Death Trooper"]
 
-    var enemies = ["Stormtrooper", "Imperial Guard", "Scout Trooper"]
-    randEnemy = randEnemies(enemies)
 
 
     gainItem = () =>{
@@ -67,10 +92,9 @@ const name = readline.question("Please input a username to begin: ", {
         console.log("You got a credit!")
         if(inventory.length == 1){
             console.log(`Inventory: ${inventory.length} credit`)
-            }else if(inventory.length == 8){
-                console.log("")
-                console.log("YOU WIN")
-                console.log("Good work! The rebellion thanks you for your efforts")
+            }else if(inventory.length == 5){
+                console.log(`Inventory: ${inventory.length} credits`)
+                win()
             }else{
                 console.log(`Inventory: ${inventory.length} credits`)
             }
@@ -91,14 +115,31 @@ const name = readline.question("Please input a username to begin: ", {
         return
     }
 
-
+//Fight
     fight = () =>{
+
         var enemyHp = 50
-        dealDamage = () =>{
+        dealDamage1 = () =>{
             enemyHp -= Math.floor(Math.random() * 20) + 10;
             if(enemyHp >= 0){
                 console.log("Enemy Health: " + enemyHp)
-                console.log(`The ${randEnemy} attacked back!`)
+                console.log(`It attacked back!`)
+                }else{ console.log("Enemy Health: 0")}
+        }
+
+        dealDamage2 = () =>{
+            enemyHp -= Math.floor(Math.random() * 10) + 10;
+            if(enemyHp >= 0){
+                console.log("Enemy Health: " + enemyHp)
+                console.log(`It attacked back!`)
+                }else{ console.log("Enemy Health: 0")}
+        }
+
+        dealDamage3 = () =>{
+            enemyHp -= Math.floor(Math.random() * 15) + 10;
+            if(enemyHp >= 0){
+                console.log("Enemy Health: " + enemyHp)
+                console.log(`It attacked back!`)
                 }else{ console.log("Enemy Health: 0")}
         }
 
@@ -114,23 +155,23 @@ const name = readline.question("Please input a username to begin: ", {
         console.log("")
 
         if(attacks[0]){
-            dealDamage()
+            dealDamage1()
             takeDamage()
         }else if(attacks[1]){
-            dealDamage()
+            dealDamage2()
              takeDamage()
         }else if(attacks[2]){
-            dealDamage()
+            dealDamage3()
             takeDamage()
         }else{takeDamage()}
 
-
+        //Win or Lose
         if( enemyHp <= 0 && hp > 0){
-            console.log(`You beat the ${randEnemy}!`)
+            console.log(`You beat the it!`)
             console.log("")
             gain()
             console.log("")
-            console.log("press 'w' to walk")
+            // console.log("press 'w' to walk")
             return
             }else if(hp <= 0){
                 die()
@@ -150,8 +191,11 @@ const name = readline.question("Please input a username to begin: ", {
         fight()}
     }
 
+    
+
    encounter = () =>{
-        console.log(`It's a ${randEnemy}`)
+        var randomEnemies = enemies[Math.floor(Math.random() * enemies.length)] 
+        console.log(`It's a ${randomEnemies}`)
         if(readline.keyInYNStrict("Fight it?")){
             fight()
             return
@@ -178,7 +222,7 @@ const name = readline.question("Please input a username to begin: ", {
 
 
     while( hp > 0){
-       var user = readline.prompt("press 'w' to walk");
+       var user = readline.keyIn([" "[" w"]]);
        if(user === "w"){
         walk()
         console.log("")
@@ -189,19 +233,23 @@ const name = readline.question("Please input a username to begin: ", {
             console.log(`Inventory: ${inventory.length} credit`)
             console.log(`Current health: ${hp}`)
             console.log("")
-            console.log("Press 'w' to walk")
+            // console.log("Press 'w' to walk")
         }else{
             console.clear()
             console.log(name)
             console.log(`Inventory: ${inventory.length} credits`)
             console.log(`Current health: ${hp}`)}
+            console.log("")
        }else if(user === "x"){
         console.clear()
         if(readline.keyInYNStrict("Are you sure you want to exit?")){
+            console.clear()
             return
-        }else{
+        }
+        else{
             console.log("")
-            console.log("Press 'w' to walk")}
+            console.log("Press 'w' to walk")
+        }
        }else{
         console.log("press 'w' to walk")}
     }
